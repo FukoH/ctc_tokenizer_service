eae6969f2fa06465f40a65edcdf581918908cb79
Huang shengxin <hsx500@163.com>
2018-09-10 20:28:54 +0800

    test

--- app/main/CtcTokenizer.py
+++ app/main/CtcTokenizer.py
@@ -1,3 +1,12 @@
+import jieba
+import config
+jieba.initialize()
 
 class Tokenizer:
-    pass
+
+    def __init__(self,dict_path):
+        jieba.load_userdict(dict_path)
+        jieba.initialize()
+
+    def cut(self,sentence):
+        jieba.cut(sentence,)
---
+++ app/models.py
@@ -0,0 +1,36 @@
+
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Vocabulary(Base):
+
+
+    __tablename__ = 'vocabulary'
+
+    id = Column(Integer, primary_key=True)
+    value = Column(String(32), nullable=False)
+    type = Column(Integer, primary_key=False)
+    sub_type = Column(String(32), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'value':self.value,
+            'type':self.type,
+            'sub_type':self.sub_type
+        }
+
+
+#TODO 连接数据库
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+
+Base.metadata.create_all(engine)
